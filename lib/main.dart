259e54862aba2b604094fc036dd0953dbeb6213c
AlessandraRdz <a20490230@itmexicali.edu.mx>
2023-11-01 22:24:43 -0700

    ahora si

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:triki_triki/config/router/app_router.dart';
 import 'package:triki_triki/config/theme/app_theme.dart';
 import 'package:triki_triki/presentation/screen/home/home_screen.dart';
 
@@ -12,9 +13,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
         debugShowCheckedModeBanner: false,
         theme: AppTheme(selectedColor: 4).gettheme(),
-        home: const HomeScreen());
+        routerConfig: appRouter);
   }
 }
